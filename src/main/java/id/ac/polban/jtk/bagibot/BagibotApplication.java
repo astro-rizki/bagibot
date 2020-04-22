2abb178d2731c72079989c2371fd5f2897accc11
astro-rizki <srizkiafrizal85@gmail.com>
2020-04-22 20:49:26 +0700

    Init
    First commit to init spring project

---
+++ src/main/java/id/ac/polban/jtk/bagibot/BagibotApplication.java
@@ -0,0 +1,13 @@
+package id.ac.polban.jtk.bagibot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BagibotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BagibotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/ac/polban/jtk/bagibot/BagibotApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.polban.jtk.bagibot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BagibotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
